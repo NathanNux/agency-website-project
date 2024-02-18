51562181105190b9eafe154864e1831245ca3d93
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-02-18 20:12:30 +0100

    Navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+"use client"
+import React, { useRef } from 'react';
 import Image from 'next/image';
 import Navbar from '@/components/Navbar';
 
-export default function Homw() {
+export default function Home() {
+
+  const websiteDesignRef = useRef<HTMLDivElement>(null);
+  const graphicDesignRef = useRef<HTMLDivElement>(null);
+  const shopifyStoresRef = useRef<HTMLDivElement>(null);
+  const brandsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToWebsiteDesign = () => {
+    websiteDesignRef.current?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest", });
+  }
+  
+  const scrollToGraphicDesign = () => {
+    graphicDesignRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  const scrollToShopifyStores = () => {
+    shopifyStoresRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  const scrollToBrands = () => {
+    brandsRef.current?.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className='w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02 relative overflow-hidden]'>
-      <Navbar />
+      <Navbar 
+        scrollToWebsiteDesign={scrollToWebsiteDesign}
+        scrollToGraphicDesign={scrollToGraphicDesign}
+        scrollToShopifyStores={scrollToShopifyStores}
+        scrollToBrands={scrollToBrands}
+      />
     </div>
   );
 }
